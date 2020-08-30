23114b51f6dd6a24344c7b3e828dbee251b89b45
ahmedlhassan <ah.hassan@nu.edu.eg>
2020-08-30 02:24:35 +0200

    singup

--- mr-syed/settings.py
+++ mr-syed/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # local apps
     'home',
-    'profiles'
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- mr-syed/urls.py
+++ mr-syed/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')), # new
+    path('accounts/', include('accounts.urls')), # new
     path('', include('home.urls')),
-   
 ]

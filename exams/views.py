dcac1b7886837dbd9d0332350fb7b9749b176ed7
yasser <yasseraboelgheit@weaccept.co>
2020-09-05 12:22:42 +0200

    delete old code

--- exams/views.py
+++ exams/views.py
@@ -69,6 +69,9 @@ class ExamListView(ListView):
             all_exams["exam"] = {"exam": i,
                                  "answered": answered}
         ctx["exams"] = all_exams
+        if self.request.session.get('exam_time_expired'):
+            ctx["exam_time_expired"] = True
+            del self.request.session["exam_time_expired"]
         return ctx
 
 
@@ -84,6 +87,7 @@ class QuestionUpdateView(UpdateView):
         addtime = timedelta(minutes=self.exam.time_quiz)
         finish_time = self.student_exam.created_at + addtime
         if finish_time < timezone.now():
+            self.request.session['exam_time_expired'] = True
             return redirect("exam_list")
         return super().dispatch(request, *args, **kwargs)
 
@@ -96,8 +100,7 @@ class QuestionUpdateView(UpdateView):
 
     def get_object(self):
 
-        exam = Exam.objects.get(id=self.kwargs.get("exam_pk"))
-        all_questions = get_all_questions(exam.id)
+        all_questions = get_all_questions(self.exam.id)
         question_content = all_questions[self.kwargs.get("question_pk")]
 
         # get or create student answer
@@ -110,22 +113,14 @@ class QuestionUpdateView(UpdateView):
 
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
-        exam = Exam.objects.get(id=self.kwargs.get("exam_pk"))
-        all_questions = get_all_questions(exam.id)
+        all_questions = get_all_questions(self.exam.id)
         ctx["question_content"] = all_questions[self.kwargs.get("question_pk")]
-        ctx["exam"] = exam
+        ctx["exam"] = self.exam
         ctx["all_questions"] = all_questions
-        ctx["exam_time"] = exam.time_quiz
+        ctx["exam_time"] = self.exam.time_quiz
         ctx["student_exam"] = self.student_exam
         ctx["question_id"] = self.kwargs.get("question_pk")
-
         return ctx
-
-    # def get_context_data(self, *args, **kwargs):
-    #     ctx = super().get_context_data(*args, **kwargs)
-    #     ctx["question"] = ChoiceQuestion.objects.get(id=self.kwargs.get(
-    #         "question_pk"), exam__id=self.kwargs.get("exam_pk"))
-    #     return ctx
 
     def get_success_url(self):
         all_questions = get_all_questions(self.kwargs.get("exam_pk"))
@@ -139,76 +134,3 @@ class QuestionUpdateView(UpdateView):
 
         return reverse_lazy('question', kwargs={'question_pk': question_number,
                                                 "exam_pk": self.kwargs.get("exam_pk")})
-
-
-# class ChoiceQuestionUpdateView(UpdateView):
-#     # model = StudentChoiceAnswer
-#     template_name = 'exams/essay-question.html'
-#     form_class = StudentChoiceAnswerForm
-
-#     def get_object(self):
-#         exam = Exam.objects.get(id=self.kwargs.get("exam_pk"))
-#         all_questions = get_all_questions(exam.id)
-#         question = all_questions[self.kwargs.get("question_pk")]["question"]
-#         student_exam, created = StudentExam.objects.get_or_create(
-#             user=self.request.user, exam=exam)
-#         answer, created = StudentChoiceAnswer.objects.get_or_create(
-#             question=question, student_exam=student_exam, )
-
-#         return answer
-
-#     def form_valid(self, form):
-#         return super().form_valid(form)
-
-#     def get_context_data(self, *args, **kwargs):
-#         ctx = super().get_context_data(*args, **kwargs)
-#         exam = Exam.objects.get(id=self.kwargs.get("exam_pk"))
-#         all_questions = get_all_questions(exam.id)
-#         ctx["question_content"] = all_questions[self.kwargs.get("question_pk")]
-#         ctx["exam"] = exam
-#         ctx["all_questions"] = all_questions
-#         return ctx
-
-#     def get_success_url(self):
-#         all_questions = get_all_questions(self.kwargs.get("exam_pk"))
-#         question_number = self.kwargs.get("question_pk")
-#         question = all_questions[question_number]["question"]
-
-#         if question_number == len(all_questions):
-#             question_number = 1
-#         else:
-#             question_number += 1
-
-#         return reverse_lazy(all_questions[question_number]["type"], kwargs={'question_pk': question_number, "exam_pk": self.kwargs.get("exam_pk")})
-
-
-# class ExamDetailView(DetailView):
-#     model = Exam
-#     template_name = "exams/exam.html"
-
-#     def get_context_data(self, **kwargs):
-#         exam = self.get_object()
-#         context = super().get_context_data(**kwargs)
-#         choice_qs = ChoiceQuestion.objects.filter(exam=exam)
-#         essay_qs = EssayQuestion.objects.filter(exam=exam)
-#         true_false_qs = TrueFalseQuestion.objects.filter(exam=exam)
-#         choice_qs_dict = OrderedDict()
-#         choice_essay_dict = OrderedDict()
-#         true_false_qs_dict = OrderedDict()
-
-#         question_number = 1
-
-#         for q in range(choice_qs.count()):
-#             choice_qs_dict[str(question_number)] = choice_qs[q]
-#             question_number = question_number + 1
-#         for q in range(essay_qs.count()):
-#             choice_essay_dict[str(question_number)] = essay_qs[q]
-#             question_number = question_number + 1
-#         for q in range(true_false_qs.count()):
-#             true_false_qs_dict[str(question_number)] = true_false_qs[q]
-#             question_number = question_number + 1
-
-#         context["choice_qs"] = choice_qs_dict
-#         context["essay_qs"] = choice_essay_dict
-#         context["true_false_qs"] = true_false_qs_dict
-#         return context

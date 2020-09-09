4aec0c678409c3f765164c4f8d18925603ec4eda
yasser <yasseraboelgheit@weaccept.co>
2020-09-10 00:48:14 +0200

    lectures

--- mr-syed/urls.py
+++ mr-syed/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('exams/', include('exams.urls')),
     path('homeworks/', include('homework.urls')),
+    path('lectures/', include('lectures.urls')),
     path('', include('home.urls')),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

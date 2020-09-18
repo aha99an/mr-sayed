ba97745e33e88b6ee673058a7d8255bc8f694942
ahmedlhassan <ah.hassan@nu.edu.eg>
2020-09-18 03:14:09 +0200

    save work

--- questions/models.py
+++ questions/models.py
@@ -4,13 +4,12 @@ from accounts.models import CustomUser
 # Create your models here.
 
 class MrQuestion(models.Model):
-    ءuser = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE, null=True, blank=True , related_name="user_mr_question")
+    user = models.ForeignKey(
+    CustomUser, on_delete=models.CASCADE, null=True, blank=True , related_name="user_mr_question")
     question = models.CharField(max_length=1000, null=True, blank=True)
+    image_question = models.ImageField(null=True, blank=True)
     answer = models.CharField(max_length=1000, null=True, blank=True)
-
-
-
-
+    image_answer = models.ImageField(null=True, blank=True)
+    is_answered = models.BooleanField(default=True) ##?? (default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

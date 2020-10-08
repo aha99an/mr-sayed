dd3a8befa16097f5478bccdb7c03e456e19ebea4
yasser <yasseraboelgheit@weaccept.co>
2020-10-08 18:32:51 +0200

    test performance

--- mr-syed/storages.py
+++ mr-syed/storages.py
@@ -1,7 +1,7 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
-    
+
     def _get_write_parameters(self, name, content=None):
         params = super()._get_write_parameters(name, content)
         if hasattr(self, 'default_acl'):

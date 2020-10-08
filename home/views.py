c387ba5b3f6622abb0363b9a9994ed3e42e2d4db
yasser <yasseraboelgheit@weaccept.co>
2020-10-08 04:34:09 +0200

    test performance

--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView, CreateView
 
 class HomePageView(TemplateView):
     template_name = 'home/home.html'
+
+class TestView(TemplateView):
+    template_name = "test/elements.html"

5a2e66e9b6fad07f16900ae2f2da6bbe5329ab35
yasser <yasseraboelgheit@weaccept.co>
2020-10-09 19:02:16 +0200

    test heroku shell

--- s3-no-enc.py
+++
@@ -1,7 +0,0 @@
-class StaticStorage(S3Boto3Storage):
-    
-    def _get_write_parameters(self, name, content=None):
-        params = super()._get_write_parameters(name, content)
-        if hasattr(self, 'default_acl'):
-            params.update({'ACL': 'public-read'})
-        return params

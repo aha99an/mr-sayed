f01444dd7ab50ea50c9539d778655bb0eebf5c8d
yasser <yasseraboelgheit@weaccept.co>
2020-10-10 03:31:30 +0200

    first_name max_length

--- accounts/models.py
+++ accounts/models.py
@@ -14,7 +14,7 @@ class CustomUser(AbstractUser):
     )
 
     username = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=1000)
     last_name = models.CharField(max_length=200)
     school = models.CharField(max_length=200)
     parentPhoneNumber = models.IntegerField(default=0)

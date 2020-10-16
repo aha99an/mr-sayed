b642a0a03da322e752da2b6012ef9ff8ae5a24c2
yasser <yasseraboelgheit@weaccept.co>
2020-10-16 11:20:50 +0200

    fix lectures view

--- lectures/views.py
+++ lectures/views.py
@@ -55,7 +55,7 @@ class LectureDetailView(StudentPermission, DetailView):
 
                 return super().dispatch(request, *args, **kwargs)
 
-            if student_class.week_day == now.weekday() and student_class.start < now.time():
+            if student_class.week_day == now.weekday() and student_class.start <= now.time():
                 now_minus_start_minutes = check_lecture_time(self.request.user)
                 if self.object.lecture_allowed_time > now_minus_start_minutes and now.date() < self.object.week.end:
                     return super().dispatch(request, *args, **kwargs)

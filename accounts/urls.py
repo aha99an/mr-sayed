46dd52a3ce0fcc309f47ad57901a12e5ce165c5f
Ahmad Hassan <ah.hassan@nu.edu.eg>
2020-11-30 17:59:06 +0200

    addLastExamGrade&LastHomeworkGrade

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,21 +1,24 @@
 from django.urls import path
 from .views import (SignUpView, ProfileView, ExamQuestionDetailView,
                     HomeworkDetailView, MyProfileDataUpdateView, IsNotActiveTemplateView)
-from .views_admin import (AdminStudentListView, AdminStudentUpdateView, reset_password,
+from .views_admin import (AdminStudentListView, AdminStudentUpdateView, reset_password,AdminProfileView,
                           add_makeup_lecture, LectureMakeupDeleteView, AdminAccountDeleteView, AdminMyProfileData,
                           AdminMyProfileDataUpdateView, ExamMakeupDeleteView, add_makeup_exam, StudentPaymentUpdateView,
                           AdminStudentPaymentListView, StudentPaymentCreateView, AdminStudentPaymentDeleteView,
-                          add_makeup_homework, HomeworkMakeupDeleteView)
+                          add_makeup_homework, HomeworkMakeupDeleteView, AdminHomeworkDetailView)
 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('admin-profile/<int:pk>', AdminProfileView.as_view(), name='admin_profile'),
     path('students/', AdminStudentListView.as_view(), name='admin_student_list'),
     path('profile-exam/<int:student_exam_pk>/<int:question_pk>/',
          ExamQuestionDetailView.as_view(), name="profile_exam_question"),
     path('profile-homework/<int:pk>/',
          HomeworkDetailView.as_view(), name="profile_homework"),
+    path('admin-profile-homework/<int:pk>/',
+         AdminHomeworkDetailView.as_view(), name="admin_profile_homework"),
     path("student/<int:pk>", AdminStudentUpdateView.as_view(),
          name="student_update_view"),
     path("admin-reset-password/<int:pk>", reset_password,

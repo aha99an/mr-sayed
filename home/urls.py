8e31121807c9dba15965cda5fcc6a2e76c2e4f8c
yasser aboelgheit <yasser.aboelgheit@gmail.com>
2021-06-27 19:12:26 +0200

    new ssl

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from home.views import ahmedHassan
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('.well-known/acme-challenge/tCOO5IQWjGbxSNUUnZTWxLUQ2U6SuPknNVCQwX9B6Bw/', TestPageView.as_view(), name="test"),
+    path('.well-known/acme-challenge/V9PvGlFDugxa4JrhHqf4Jrmk6ZJ1KOPwfl7VMuZNNUk/', TestPageView.as_view(), name="test"),
     path('ahmed-hassan', ahmedHassan.as_view(), name='ahmed_hassan'),
 
 ]

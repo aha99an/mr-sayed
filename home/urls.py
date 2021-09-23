cc52e50459475c5c01c1629fc084876d62bf976a
yasser aboelgheit <yasser.aboelgheit@gmail.com>
2021-09-24 01:20:53 +0200

    renew certificate

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from home.views import ahmedHassan
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('.well-known/acme-challenge/pA_0B3Uj9jTNr_Z3fu4_9103qob8fUF--OZSamNeWB4/', TestPageView.as_view(), name="test"),
+    path('.well-known/acme-challenge/0iDuM9BMYqxmU4d0k7ZaDBc_lQJxClXZFlGO-HrZxVM', TestPageView.as_view(), name="test"),
     path('ahmed-hassan', ahmedHassan.as_view(), name='ahmed_hassan'),
 
 ]
